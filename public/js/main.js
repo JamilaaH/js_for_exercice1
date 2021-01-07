4f8ff4e49fc5e700d4acaf2bbf61fc88dfd2f7af
Jamila <jaja_mimi@hotmail.com>
2021-01-07 16:55:19 +0100

    exo for

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+// exo1
+
+// for (let i = 0; i <= 20 ; i++) {
+//     switch (i % 2 == 0) {
+//         case true:
+//             console.log(`${i} est un nombre paire`);
+//             break;
+//         case false:
+//             console.log(`${i} est un nombre impaire`);
+//             break;
+//     }    
+// }
+
+// exo 2
+
+// let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"];
+
+// for (let i = 0; i < coding16.length; i++) {
+//     console.log(`Bonjour ${coding16[i]}`);
+    
+// }
+
+// exo 3
+
+let nbr = parseInt(prompt("jusqu'a quel chiffre voulez vous y aller ?"));
+
+for (let i = 0; i <= nbr; i++) {
+    console.log(i);
+    
+}
